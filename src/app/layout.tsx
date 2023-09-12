e7475cf1478ee0e609ae726c5333b8ebef251649
gabyrodrigues <gabyrlima.99@gmail.com>
2023-09-11 21:28:00 -0300

    creating globalStyles + improving tests with jest-styled-components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import StyledComponentsRegistry from "@lib/registry";
+import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <Providers>{children}</Providers>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -10,4 +10,12 @@ describe("<Main />", () => {
     expect(screen.getByRole("heading", { name: /react avançado/i })).toBeInTheDocument();
     expect(container.firstChild).toMatchSnapshot();
   });
+
+  it("should render the colors correctly", () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toHaveStyle({
+      "background-color": "#06092b"
+    });
+  });
 });
